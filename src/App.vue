903bbf095adbce3309b771eb6303b7e2371768f5
lucasmjuarez <lumas89@gmail.com>
2020-05-25 20:17:01 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,234 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="app"
+    :class="typeof weather.main != 'undefined' && 
+  weather.main.temp > 16 ? 'warm' : ''"
+  >
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+          <div class="date">{{dataBuilder()}}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
+          <div class="humidity">Humedad: {{weather.main.humidity}}%</div>
+          <div class="minima">Minima del día: {{Math.round(weather.main.temp_min)}}°c</div>
+          <div class="maxima">Máxima del día: {{Math.round(weather.main.temp_max)}}°c</div>
+          <div class="speed">Velocidad del viento: {{weather.wind.speed}} km/h</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      api_key: "8d4413b8e5d8f7668063f2aaebfb37b7",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {}
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`
+        )
+          .then(res => {
+            const data = res.json();
+            console.log(data);
+            return data;
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dataBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    }
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: cover;
+  background-position: botton;
+  transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(
+    to botton,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 20px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: solid;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 10px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 12px, rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 16px 16px 16px;
+}
+
+.location-box .location {
+  color: rgb(27, 34, 4);
+  font-size: 32px;
+  font-weight: 500;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-shadow: 1px 2px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: rgb(20, 14, 14);
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+  text-shadow: 1px 2px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: rgb(29, 27, 27);
+  font-size: 100px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 16px;
+  margin: 40px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.5);
+}
+
+.weather-box .weather {
+  color: rgb(5, 8, 12);
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3p 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .humidity {
+  color: rgb(5, 8, 12);
+  font-size: 36px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3p 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .minima {
+  color: rgb(5, 8, 12);
+  font-size: 36px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3p 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .maxima {
+  color: rgb(5, 8, 12);
+  font-size: 36px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3p 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .speed {
+  color: rgb(5, 8, 12);
+  font-size: 36px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3p 6px rgba(0, 0, 0, 0.25);
 }
 </style>
